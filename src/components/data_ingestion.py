d8d51b093a0fbe5d0e75844bebf0f57dc0e422cd
sonicsomanna <sonicsomanna004@gmail.com>
2024-01-30 10:14:41 +0530

    Data Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -4,6 +4,9 @@ from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 from dataclasses import dataclass
 from sklearn.model_selection import train_test_split
 
@@ -51,3 +54,6 @@ class DataIngestion:
 if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
